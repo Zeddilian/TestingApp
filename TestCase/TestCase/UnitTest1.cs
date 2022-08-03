de74145a00fc0d855b4dd5a3278b995ff62dd67f
Alexandro <some@mail.com>
2022-08-03 18:33:19 +0300

    test added

---
+++ TestCase/TestCase/UnitTest1.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using System;
+
+namespace TestCase
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Random rnd = new Random();
+
+            int number = rnd.Next(500, 1001);
+
+            Assert.IsTrue(IsInRange(number), "Number is not in the range (600, 900)");
+        }
+
+        private bool IsInRange(int number)
+        {
+            if (number >= 600 && number <= 900)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
